a2a80099277a71d50a999116639f97c518e223ab
tamaki0506 <tamaki.tetsumoto@eure.jp>
2017-01-23 16:32:58 +0900

    add nil check

--- client/transaction/transaction_search.go
+++ client/transaction/transaction_search.go
@@ -113,6 +113,9 @@ func (r *TransactionSearchResponse) TransactionIDList() (list []string) {
 
 // GetProfileCreatedDate returns time of profile created.
 func (r *TransactionSearchResponse) GetProfileCreatedDate() (time.Time, bool) {
+	if len(r.Items) == 0 {
+		 return time.Time{}, false
+	}
 	item := r.Items[0]
 	if item.IsProfile() && item.IsCreated() {
 		return item.Timestamp, true
